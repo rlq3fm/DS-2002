ef92c916e6c12c6454eb55d607eeee66b9a01a03
unknown <rlq3fm@virginia.edu>
2022-12-06 09:49:28 -0500

    projects 1 and 2

---
+++ Projects/chinook-source-data/STREAM/stream_queries.sql
@@ -0,0 +1,49 @@
+# 1
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 0;
+
+# 2
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 224;
+
+# 3
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 448;
+
+# 4
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 672;
+
+# 5
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 896;
+
+# 6
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 1120;
+
+# 7
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 1344;
+
+# 8
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 1568;
+
+# 9
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 1792;
+
+# 10
+SELECT * FROM chinook_dw.fact_invoices
+ORDER BY fact_invoice_key ASC 
+LIMIT 224 offset 2016;
